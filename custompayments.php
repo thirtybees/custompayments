b8ca41e597d9cdceadd56be23cb00cf09489fe04
Michael Dekker <git@michaeldekker.nl>
2018-04-25 08:42:12 +0200

    Bump to v1.2.3

--- custompayments.php
+++ custompayments.php
@@ -51,7 +51,7 @@ class CustomPayments extends PaymentModule
     {
         $this->name = 'custompayments';
         $this->tab = 'payments_gateways';
-        $this->version = '1.2.2';
+        $this->version = '1.2.3';
         $this->author = 'thirty bees';
         $this->need_instance = 1;
 
