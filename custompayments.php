d88582b1d25e3c6e37ba5c3868f0ae123cec87f9
Petr Hučík <petr@getdatakick.com>
2018-09-12 06:52:41 +0200

    Bump version to 1.2.4

--- custompayments.php
+++ custompayments.php
@@ -51,7 +51,7 @@ class CustomPayments extends PaymentModule
     {
         $this->name = 'custompayments';
         $this->tab = 'payments_gateways';
-        $this->version = '1.2.3';
+        $this->version = '1.2.4';
         $this->author = 'thirty bees';
         $this->need_instance = 1;
 
